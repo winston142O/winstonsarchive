3fbf37c7953a8a380c6084e65dff4a6469086d1a
Winston142O <winston1420@hotmail.com>
2023-12-19 22:18:40 -0400

    Changed secret key

--- winstonsarchive_rev/settings.py
+++ winstonsarchive_rev/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u!9k#tj=8o$#y29hip6p))pgpw3#cdu0@eyh-yubk8-x54b@fp"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
