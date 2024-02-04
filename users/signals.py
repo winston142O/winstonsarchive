b224944dd0bfabec9dc203aee0f9d06721774306
winston142O <winston1420@hotmail.com>
2024-02-04 18:41:23 -0400

    Documented signals and updated urls.py

--- users/signals.py
+++ users/signals.py
@@ -5,9 +5,13 @@ from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
+    """ Always create a profile when a user is created. """
+    
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
+    """ Always save the profile when the user is saved."""
+    
     instance.profile.save()
