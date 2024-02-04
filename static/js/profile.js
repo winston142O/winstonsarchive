7b8c3feed7dc4d0324a775f09a2b086482216c33
winston142O <winston1420@hotmail.com>
2024-02-04 18:39:39 -0400

    Added profile section, about me section, and customized alerts

---
+++ static/js/profile.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    // Get and store elements
+    var userData = document.getElementById('user-data');
+    var editUserForm = document.getElementById('edit-user-form');
+    
+    // Hide initially
+    editUserForm.classList.add('hide-div');
+
+    // Apply fade-in animations
+    userData.classList.add('fade-in', 'visible');
+    editUserForm.classList.add('fade-in');
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get and store elements
+    var editProfileBtn = document.getElementById('edit-profile');
+    var editUserForm = document.getElementById('edit-user-form');
+    var userData = document.getElementById('user-data');
+
+    // Add click event listener
+    editProfileBtn.addEventListener('click', function() {
+        editUserForm.classList.remove('hide-div');
+        setTimeout(function() {
+            editUserForm.classList.add('visible');
+            userData.classList.remove('visible');
+        }, 200);        
+    });
+});
